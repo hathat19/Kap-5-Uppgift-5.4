d952deb07f3afa85256231f22cacc9fc9f101b91
humy06001 <humy06001@E-PW0234KS>
2023-11-22 21:21:22 +0100

    Add project files.

---
+++ Kap 5 Uppgift 5.4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Uppgift5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //-------------------------------------------
+
+            //variabler
+            string[] names = { "Hugo", "Ivan", "Bert", "Håkon", "Emil", "John", "Max" };
+            string first = "";
+            bool firstLoop = true;
+
+            //går igenom varje namn
+            foreach (string name in names)
+            {
+                //Första loop?
+                if (firstLoop)
+                {
+                    first = name;
+                    firstLoop = false;
+                }
+                else
+                {
+                first = name.CompareTo(first) < 0 ? name : first;
+                }
+            }
+            Console.WriteLine($"Namnet {first} kommer först i bokstavsordning.");
+
+        }
+    }
+}
+/*Skapa ett program som innehåller en array med minst 5 namn. Använd en loop för att
+gå igenom arrayen och sedan skriva ut det namn som kommer först i
+bokstavsordning.*/
